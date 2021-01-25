725a3cf35e650e4cf7fead99af93ba167ca1778d
Teuku Fuad Maulana <fuad.pcd@gmail.com>
2021-01-25 21:24:34 +0700

    membuat componen header , hero dan womeBanner

---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <HeaderSayna />
+    <HeroSection />
+    <WomeBanner />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderSayna from "@/components/HeaderSayna.vue";
+import HeroSection from "@/components/HeroSection.vue";
+import WomeBanner from "@/components/WomeBanner.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HeaderSayna,
+    HeroSection,
+    WomeBanner,
+  },
+};
+</script>
